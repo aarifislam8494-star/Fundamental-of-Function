010fe46aacc039c4365a33ee6d157fd730797b5e
aarifislam8494-star <aarifislam8494@gmail.com>
2026-02-05 17:56:42 +0600

    Function return and set return value to a variable

---
+++ return.js
@@ -0,0 +1,20 @@
+function tenTimes(number){
+ const result = number * 10;
+ return result;
+ 
+}
+
+// tenTimes(5);
+const output = tenTimes(5);
+console.log('output:', output);
+
+const bigNumber = tenTimes(99);
+console.log(bigNumber);
+
+// example:
+function cutHalf(number){
+ const half = number / 2;
+ return half;
+}
+const halfNumber = cutHalf(50);
+console.log(halfNumber);
