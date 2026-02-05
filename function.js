3b7e2421d120d0ed1b2c1babef422ce129011d19
aarifislam8494-star <aarifislam8494@gmail.com>
2026-02-05 16:45:04 +0600

    Added function and function syntax

---
+++ function.js
@@ -0,0 +1,28 @@
+const price = 45;
+
+function fanoffkor(){
+ console.log('bosa thake uthe dara');
+ console.log('walk towards the switch');
+ console.log('click the switch to off the fan');
+}
+
+// call the function
+fanoffkor();
+
+
+// function name(){
+// 
+// }
+function brushTeeth(){
+  console.log('pick up the brush');
+  console.log('add paste');
+  console.log('2 min gogo and goso and fena felo')
+}
+
+//call the function
+brushTeeth();
+console.log('................');
+console.log('---------------------');
+console.log('------------');
+console.log('tom tom ta to');
+brushTeeth();
